d2ddd9c5153576f477854e1167f5a3c0cd3af1f2
Luc Perkins <lucperkins@gmail.com>
2022-08-18 13:28:52 +0300

    First commit; initial Nix setup plus two extensions

---
+++ extensions.nix
@@ -0,0 +1,14 @@
+[
+  {
+    publisher = "dzhavat";
+    name = "bracket-pair-toggler";
+    version = "0.0.2";
+    sha256 = "sha256-2u+bdXU9nU1C8X3hpi7FfI2en4mlgWRPIVzcZrgGzPo=";
+  }
+  {
+    publisher = "bbenoist";
+    name = "nix";
+    sha256 = "sha256-qwxqOGublQeVP2qrLF94ndX/Be9oZOn+ZMCFX1yyoH0=";
+    version = "1.0.1";
+  }
+]
---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Visual Studio Code extensions for Nix";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        ext = { publisher, name, version, sha256 }:
+          pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+            mktplcRef = { inherit name publisher sha256 version; };
+          };
+
+        extensions = import ./extensions.nix;
+      in
+      {
+        packages = builtins.listToAttrs (builtins.map
+          (e: {
+            name = "${e.publisher}";
+            value = {
+              "${e.name}" = ext {
+                inherit (e) publisher name version sha256;
+              };
+            };
+          })
+          extensions);
+      }
+    );
+}
