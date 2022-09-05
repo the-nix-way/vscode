d4eabcc992d03f71d062e51379aec4ae0e09c868
Luc Perkins <lucperkins@gmail.com>
2022-09-05 19:58:38 +0300

    Streamline flake

--- flake.nix
+++ flake.nix
@@ -7,27 +7,31 @@
   outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        overlays = [
+          (self: super: {
+            vsCodeExtensionHelper = { publisher, name, version, sha256 }:
+              super.vscode-utils.buildVscodeMarketplaceExtension {
+                mktplcRef = { inherit name publisher sha256 version; };
+              };
+          })
+        ];
 
-        ext = { publisher, name, version, sha256 }:
-          pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-            mktplcRef = { inherit name publisher sha256 version; };
-          };
+        pkgs = import nixpkgs {
+          inherit overlays system;
+        };
 
         extensions = import ./extensions.nix;
       in
       {
-        overlays = {
-          default = self: super: {
-            vscode-extendsions = super.vscode-extensions // self.packages;
-          };
+        overlays.default = self: super: {
+          vscode-extensions = super.vscode-extensions // self.packages;
         };
 
         packages = builtins.listToAttrs (builtins.map
           (e: {
             name = "${e.publisher}";
             value = {
-              "${e.name}" = ext {
+              "${e.name}" = pkgs.vsCodeExtensionHelper {
                 inherit (e) publisher name version sha256;
               };
             };
